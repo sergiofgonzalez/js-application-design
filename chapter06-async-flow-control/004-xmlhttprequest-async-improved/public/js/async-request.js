5ad9d9c8090e10aab1ac66209c007a1028d04a7e
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2016-01-02 22:08:58 +0100

    Add custom wrapper for HTTP requests

---
+++ chapter06-async-flow-control/004-xmlhttprequest-async-improved/public/js/async-request.js
@@ -0,0 +1,96 @@
+(function() {
+  "use strict";
+
+  /* The custom helper function for HTTP requests */
+  function httpRequest(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.addEventListener("load", function() {
+      if (req.status < 400) {
+        callback(req);
+      } else {
+        callback(null, new Error("Request failed: " + req.statusText + "(" + req.status + ")"));
+      }
+    });
+    try {
+      req.send(null);
+    } catch (error) {
+      // Execution does not get here!!!
+      callback(null, new Error("Request failed: Could not send request (-1)"));
+    }
+  }
+
+  var button = document.querySelector("button");
+  button.addEventListener("click", function() {
+    showRequestInProgress();
+    httpRequest("http://spark:8080/data.txt", showResults);
+  });
+
+  var alertBox = null;
+  function showRequestInProgress() {
+    alertBox = document.createElement("div");
+    alertBox.className = "alert alert-info";
+    alertBox.textContent = "HTTP request in progress";
+    document.querySelector("#container").appendChild(alertBox);
+  }
+
+  function appendTableData(tableRow, text) {
+    var tableData = document.createElement("td");
+    tableData.textContent = text;
+    tableRow.appendChild(tableData);
+  }
+
+  function showResults(req, error) {
+    document.querySelector("#container").removeChild(alertBox);
+    if (error) {
+      alertBox = document.createElement("div");
+      alertBox.className = "alert alert-danger";
+      alertBox.textContent = error.message;
+      document.querySelector("#container").appendChild(alertBox);
+      return;
+    }
+
+    console.log("responseText(0:80):", req.responseText.slice(0, 80));
+    console.log("status:", req.status);
+    console.log("statusText:", req.statusText);
+
+    var resultsTable = document.createElement("table");
+    resultsTable.class = "table";
+
+    var caption = document.createElement("caption");
+    caption.textContent = "Synchronous Request Results";
+
+    resultsTable.appendChild(caption);
+    resultsTable.className = "table";
+
+    var tableHead = document.createElement("thead");
+    var tableRow = document.createElement("tr");
+    appendTableData(tableRow, "Description");
+    appendTableData(tableRow, "Value");
+    tableHead.appendChild(tableRow);
+
+
+    var tableBody = document.createElement("tbody");
+
+    tableRow = document.createElement("tr");
+    appendTableData(tableRow, "response(0:80)");
+    appendTableData(tableRow, req.responseText.slice(0, 80));
+    tableBody.appendChild(tableRow);
+
+    tableRow = document.createElement("tr");
+    appendTableData(tableRow, "Status");
+    appendTableData(tableRow, req.status);
+    tableBody.appendChild(tableRow);
+
+    tableRow = document.createElement("tr");
+    appendTableData(tableRow, "Status Message");
+    appendTableData(tableRow, req.statusText);
+    tableBody.appendChild(tableRow);
+
+    resultsTable.appendChild(tableHead);
+    resultsTable.appendChild(tableBody);
+
+    var container = document.querySelector("#container");
+    container.appendChild(resultsTable);
+  }
+})();
--- chapter06-async-flow-control/README.md
+++ chapter06-async-flow-control/README.md
@@ -7,7 +7,12 @@ Illustrates the use of `XMLHttpRequest` function in a synchronous way to retriev
 ## 002-angular-xmlhttprequest-sync
 The same example as [001-hello-xmlhttprequest-sync](001-hello-xmlhttprequest-sync) but based on AngularJS for HTML templating.
 
-## 003-angular-xmlhttprequest-async
+## 003-hello-xmlhttprequest-async
+Illustrates the use of `XMLHttpRequest` function in an async way, so that the page does not become unresponsive.
+
+## 004-xmlhttprequest-async-improved
+Illustrates how difficult it is to handle async execution. In the example, a helper function is defined to wrap `XMLHttpRequest` calls. The function accepts the URL and a callback function that is called with the response.
+The function works pretty well when there are not errors, but when an error is found, it is not easy to convey the error to the caller, even when try-catch and raising exceptions are included.
 
 ## e01-http-backend
 Simple HTTP backend for the examples in this chapter. See the README.md of the project for details on the endpoints.
